a13d183b5e3aef4b6b98b4653d6a9f2fde0e873c
gpabloandres <gpabloandres@gmail.com>
2024-12-17 21:29:48 -0300

    Agrega ejemplos ipynb

--- 1_chat_models/1_1_chat_model_basic.py
+++ 1_chat_models/1_1_chat_model_basic.py
@@ -1,14 +1,17 @@
+# Importar las librerías necesarias.
 import os
 import google.generativeai as genai
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
+# Cargar las variables de entorno.
 load_dotenv()
 
+# Configurar el modelo de IA: Gemini
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", temperature=0.3)
 
+# Realizar una query al modelo.
 result = model.invoke("Cuánto es 81 divido 9?")
 
 #print("Full result: ")
